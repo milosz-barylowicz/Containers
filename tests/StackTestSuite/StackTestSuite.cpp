e6c051fb00a1dad3b4a043dff227748ec569aa7f
Milosz Barylowicz <milosz.barylowicz@gmail.com>
2023-01-12 21:56:31 +0100

    some refactors

--- tests/StackTestSuite/StackTestSuite.cpp
+++ tests/StackTestSuite/StackTestSuite.cpp
@@ -12,9 +12,7 @@ namespace containers::ut
 namespace
 {
 constexpr int DEFAULT_VALUE = 100;
-constexpr int EMPTY_STACK = 0;
 constexpr int EXPECTED_VALUE = 9;
-const auto EMPTY_VARIABLE = std::nullopt;
 constexpr size_t DEFAULT_SIZE = 10;
 
 Stack<int> BuildFilledStack(int value)
@@ -30,62 +28,13 @@ Stack<int> BuildFilledStack(int value)
 
 } // namespace anonymous
 
-using namespace ::testing;
+using namespace testing;
 
 class StackTestSuite : public Test
 {
 public:
     Stack<int> sut;
 };
-
-class EmptyStackTestSuite : public StackTestSuite
-{};
-
-TEST_F(EmptyStackTestSuite, ShouldAssignOtherStacckToEmptyOne)
-{
-    Stack<int> result = BuildFilledStack(DEFAULT_VALUE);
-    sut = result;
-
-    ASSERT_EQ(result, sut);
-}
-
-TEST_F(EmptyStackTestSuite, ShouldReturnLastElementAsNull)
-{
-    ASSERT_EQ(EMPTY_VARIABLE, sut.top());
-}
-
-TEST_F(EmptyStackTestSuite, ShouldSwapWithNonEmptyStack)
-{
-    Stack<int> other = BuildFilledStack(DEFAULT_VALUE);
-
-    sut.swap(other);
-    ASSERT_FALSE(sut.empty());
-    ASSERT_EQ(5, sut.size());
-    ASSERT_EQ(DEFAULT_VALUE + 4, sut.top());
-}
-
-TEST_F(EmptyStackTestSuite, ShouldEmplaceVariable)
-{
-    sut.emplace(10);
-
-    ASSERT_FALSE(sut.empty());
-    ASSERT_EQ(EMPTY_STACK + 1, sut.size());
-    ASSERT_EQ(10, sut.top());
-}
-
-TEST_F(EmptyStackTestSuite, ShouldPushVariable)
-{
-    sut.push(DEFAULT_VALUE);
-
-    ASSERT_EQ(EMPTY_STACK + 1, sut.size());
-    ASSERT_FALSE(sut.empty());
-}
-
-TEST_F(EmptyStackTestSuite, ShouldReturnIsEmpty)
-{
-    ASSERT_EQ(EMPTY_STACK, sut.size());
-    ASSERT_TRUE(sut.empty());
-}
 
 class NonEmptyStackTestSuite : public StackTestSuite
 {
