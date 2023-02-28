44bf7db4020a2ce912ac64555b0670d6739b4e91
Milosz Barylowicz <milosz.barylowicz@gmail.com>
2023-02-28 21:18:03 +0100

    added List boilerplate code

---
+++ include/List.hpp
@@ -0,0 +1,19 @@
+/*
+ * Author: Milosz Barylowicz
+ * Date:   2022-2023
+ */
+
+#pragma once
+
+namespace containers
+{
+
+template<typename T>
+class List
+{
+public:
+private:
+    T* m_data = nullptr;
+};
+
+} // namespace containers
--- scripts/run_all_unit_tests.sh
+++ scripts/run_all_unit_tests.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # Author: Milosz Barylowicz
 # Date:   2022-2023
 
-#!/bin/bash
-
-./StackTestSuite/StackTestSuite;
+# TODO: redesign this part to run all tests without adding new element of calling here
 ./ArrayTestSuite/ArrayTestSuite;
+./ListTestSuite/ListTestSuite;
+./StackTestSuite/StackTestSuite;
 ./VectorTestSuite/VectorTestSuite;
