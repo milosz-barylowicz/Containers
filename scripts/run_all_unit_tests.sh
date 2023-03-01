983476db4bea376eedd576955a340a1ec902b2f8
Milosz Barylowicz <milosz.barylowicz@gmail.com>
2023-03-01 14:07:20 +0100

    DEBUG: changes way of running all unit tests and failed one to see if job will be broken

--- scripts/run_all_unit_tests.sh
+++ scripts/run_all_unit_tests.sh
@@ -3,9 +3,20 @@
 find_command="find"." -name '*test_suite' -type f"
 test_to_run=($(eval "${find_command}"))
 
+failed_tests="failed test cases: "
+result="0"
 for test in ${test_to_run[@]}; do
-	pushd ${test}
-	echo "Running ${test} ..."
-	./`basename ${test}`
-	popd
+    pushd `dirname ${test}`
+    echo "Running ${test}..."
+    ./`basename ${test}`
+    if [ "$?" != "0" ]; then
+        failed_tests+=" `basename ${test}`\n"
+        result="1"
+    fi
+    popd
 done
+
+if [ "${result}" != "0" ]; then
+    printf "${failed_tests}"
+fi
+exit "${result}"
