14457ae7336e50caaa0e513ab7a8686a31d2a864
Milosz Barylowicz <milosz.barylowicz@gmail.com>
2024-02-22 19:37:51 +0100

    code coverage script replaced with python script

---
+++ scripts/generate_code_coverage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+
+home_dir = os.getcwd()
+
+os.chdir('tests')
+subprocess.run(['python', 'run_unit_tests.py'])
+
+os.chdir(home_dir)
+subprocess.run(['lcov', '-c', '-d', '.', '-o', 'code-coverage.info', '--include', '*.cpp', '--include', '*.hpp'])
+subprocess.run(['genhtml', 'code-coverage.info', '--output-directory', 'code_coverage'])
