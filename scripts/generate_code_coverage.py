3aca7b6b3854a5c14c23bf160fb71b43381c5c96
Milosz Barylowicz <milosz.barylowicz@gmail.com>
2025-07-16 23:12:11 +0200

    [DevOps]: filter out external code from code-coverage raport to focus only on project source code

--- scripts/generate_code_coverage.py
+++ scripts/generate_code_coverage.py
@@ -9,5 +9,33 @@ os.chdir('tests')
 subprocess.run(['python', 'run_unit_tests.py'])
 
 os.chdir(home_dir)
-subprocess.run(['lcov', '-c', '-d', '.', '--remove', 'libreoffice_total.info', 'tests/*', '-o', 'code-coverage.info', '--include', '*.cpp', '--include', '*.hpp', '--ignore-errors', 'mismatch'])
-subprocess.run(['genhtml', 'code-coverage.info', '--output-directory', 'code_coverage'])
+
+coverage_info = 'code-coverage.info'
+
+subprocess.run([
+    'lcov',
+    '--directory', '.',
+    '--capture',
+    '--output-file', coverage_info,
+    '--ignore-errors', 'mismatch'
+], check=True)
+
+subprocess.run([
+    'lcov',
+    '--extract', coverage_info,
+    '*/include/*',
+    '--output-file', coverage_info
+], check=True)
+
+subprocess.run([
+    'lcov',
+    '--remove', coverage_info,
+    '*/gtest/*',
+    '*/usr/*',
+    '--output-file', coverage_info
+], check=True)
+
+subprocess.run([
+    'genhtml', coverage_info,
+    '--output-directory', 'code-coverage'
+], check=True)
